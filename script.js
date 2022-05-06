8e479dfb7d592e817e17f0da340fa326266d925d
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-05-06 12:01:13 -0700

    Got error messages and input border to work

---
+++ script.js
@@ -0,0 +1,50 @@
+const button = document.querySelector('button');
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const password = document.getElementById('password');
+const email = document.getElementById('email');
+const fnameError = document.getElementById('fname-error');
+const lnameError = document.getElementById('lname-error');
+const emailError = document.getElementById('email-error');
+const passwordError = document.getElementById('password-error');
+const svg = document.querySelectorAll('svg');
+
+button.addEventListener('click', (e) => {
+    console.log('click')
+
+    const validateEmail = function () {
+        const mailformat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      
+        if (email.value.match(mailformat)) {
+            return true;
+        } else {
+            return false;
+        }
+      };
+
+    if (fname.value === '' || fname.value == null) {
+        fnameError.classList.remove('hide');
+        fname.classList.add('red-border');
+        e.preventDefault();
+    } 
+    
+    if (lname.value === '' || lname.value == null) {
+        lnameError.classList.remove('hide');
+        lname.classList.add('red-border');
+        e.preventDefault();
+    } 
+    
+    if (password.value === '' || password.value == null) {
+        passwordError.classList.remove('hide');
+        password.classList.add('red-border');
+        e.preventDefault();
+    }
+
+    if (validateEmail() === false) {
+        e.preventDefault();
+        emailError.classList.remove('hide');
+        email.classList.add('red-border');
+    }
+
+})
+
