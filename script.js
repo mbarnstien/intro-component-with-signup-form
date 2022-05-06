6359d65d42bc0a735208f2e2472ded5488657f7e
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-05-06 12:17:22 -0700

    Error label workinggit add -A

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ const fnameError = document.getElementById('fname-error');
 const lnameError = document.getElementById('lname-error');
 const emailError = document.getElementById('email-error');
 const passwordError = document.getElementById('password-error');
-const svg = document.querySelectorAll('svg');
+const fnameIcon = document.getElementById('fname-icon');
+const lnameIcon = document.getElementById('lname-icon');
+const emailIcon = document.getElementById('email-icon');
+const passwordIcon = document.getElementById('password-icon');
 
 button.addEventListener('click', (e) => {
     console.log('click')
@@ -25,18 +28,21 @@ button.addEventListener('click', (e) => {
     if (fname.value === '' || fname.value == null) {
         fnameError.classList.remove('hide');
         fname.classList.add('red-border');
+        fnameIcon.classList.remove('hide');
         e.preventDefault();
     } 
     
     if (lname.value === '' || lname.value == null) {
         lnameError.classList.remove('hide');
         lname.classList.add('red-border');
+        lnameIcon.classList.remove('hide');
         e.preventDefault();
     } 
     
     if (password.value === '' || password.value == null) {
         passwordError.classList.remove('hide');
         password.classList.add('red-border');
+        passwordIcon.classList.remove('hide');
         e.preventDefault();
     }
 
@@ -44,6 +50,7 @@ button.addEventListener('click', (e) => {
         e.preventDefault();
         emailError.classList.remove('hide');
         email.classList.add('red-border');
+        emailIcon.classList.remove('hide');
     }
 
 })
